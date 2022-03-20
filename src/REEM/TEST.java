765fb2885af483e3ede67869e8fda7a5e53d6bc9
Reemnabhan <98900968+Reemnabhan@users.noreply.github.com>
2022-03-20 18:32:03 +0100

    test procject

---
+++ src/REEM/TEST.java
@@ -0,0 +1,11 @@
+package REEM;
+
+public class TEST {
+
+    public static void main(String[] args){
+    int num1=200;
+    int num2=20;
+    System.out.println("num1+num2:" + (num1+num2));
+}
+
+}
